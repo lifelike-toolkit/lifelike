b2b59a80bc99ea076ac9c0393aede91d303c1917
Khoa Nguyen <dangkhoa81122@gmail.com>
2023-04-25 01:40:29 -0400

    /bin/python3 /home/khoa/Documents/Projects/lifelike/toolkit/embeddingTuner.py

--- toolkit/embeddingTuner.py
+++ toolkit/embeddingTuner.py
@@ -4,9 +4,11 @@ Tunes sequence embeddings to allow the game to more accurately predict player's 
 """
 import numpy
 import torch
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
 # Import model and tokenizer here
-model = AutoModel.from_pretrained('../models/bertGoEmotion') # Assumes the configuration is correct for the use case
+model = AutoModelForSequenceClassification.from_pretrained('models/') # Assumes the configuration is correct for the use case
 tokenizer = AutoTokenizer.from_pretrained('microsoft/xtremedistil-l6-h384-uncased') # change parameter to your own provided pre-trained model
 
+sentimentAnalysis = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer, top_k=28)
+print(sentimentAnalysis("I hate you"))
