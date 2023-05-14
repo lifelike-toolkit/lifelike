434dd33fc26cc78bf2381716b092b38a49a1bcd6
Khoa Nguyen <dangkhoa81122@gmail.com>
2023-05-14 03:10:21 -0400

    Minor fix

--- lifelike/StateManager/knowledge_tree.py
+++ lifelike/StateManager/knowledge_tree.py
@@ -11,7 +11,7 @@ from lifelike.StateManager.base_game_tree import BaseGameTree
 class KnowledgeTree(BaseGameTree):
     """Allows for loading the tree from a pre-defined list of contextual texts"""
     @classmethod
-    def from_texts(cls: BaseGameTree, name:str, texts: list[str], embedding_function: Embeddings, metadatas: list[dict]=None, ids: list[str]=None):
+    def from_texts(cls: BaseGameTree, name:str, texts: list[str], embedding_function: Embeddings, metadatas: list[dict]=None, ids: list[str]=None) -> 'KnowledgeTree':
         """For now, must know the embedding dimension."""
         if ids is None: # If id not provided, generate it
             ids = [str(uuid.uuid1()) for _ in texts]
