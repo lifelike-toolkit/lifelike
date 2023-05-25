9f918dff8eb5e8c0b3c171a38d4881c30947f394
mustafa-tariqk <mustafa@tariqk.tech>
2023-05-25 14:58:02 -0700

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lifelike',
-    version='1.0.6',
+    version='1.0.7',
     description='A A toolkit that allows for the creation of "lifelike" characters that you can interact with and change how they behave towards you',
     author='Mustafa Tariq and Khoa Nguyen',
     license='MIT',
